8709a026513970d421b44d94162d55fa2969f957
Neville <nevilleneville@hotmail.com>
2019-01-14 14:07:04 -0600

    add navbar

--- src/js/CanvasGalleryApp.js
+++ src/js/CanvasGalleryApp.js
@@ -7,8 +7,9 @@ import Navbar from "./Navbar";
 class CanvasGalleryApp extends React.Component {
     render() {
         return (
-            <div>
+            <div className="position-relative">
                 <Drawing drawingID={0}/>
+                <Navbar pageNumber={0+1}/>
             </div>
         );
     }
--- src/js/Navbar.js
+++ src/js/Navbar.js
@@ -1,8 +1,25 @@
 import React from "react";
 
-const NavBar = () => {
+const NavBar = (props) => {
     return (
-        <nav className="navbar">
+        <nav className="nav">
+            <ul className="nav__navbar">
+                <li>
+                    <a href="#">First</a>
+                </li>
+                <li>
+                    <a href="#">Previous</a>
+                </li>
+                <li>
+                    <input type="text" maxLength="2"className="nav__navbar__input" placeholder={props.pageNumber}></input><span>/100</span>
+                </li>
+                <li>
+                    <a href="#">Next</a>
+                </li>
+                <li>
+                    <a href="#">Last</a>
+                </li>
+            </ul>
         </nav>
     );
 }
